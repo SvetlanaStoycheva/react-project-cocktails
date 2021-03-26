1c145e7f7124d57ef174c97a1441ae83df251752
Svetlana Stoycheva <s.stoycheva@gmail.com>
2021-03-26 14:36:10 +0200

    initial commmit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h2 className='section-title'>about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        accusamus, vero asperiores eaque placeat voluptate nemo corrupti, error
+        perferendis ut omnis rem, dolore sed explicabo mollitia natus.
+        Exercitationem, odit necessitatibus!
+      </p>
+    </section>
+  )
+}
+
+export default About
